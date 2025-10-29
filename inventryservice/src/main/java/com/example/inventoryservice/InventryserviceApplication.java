6bf1fd2d0b50eb7641b31c5487bf87365c78b400
InDepthCode <amrut.work@gmail.com>
2025-10-29 22:31:55 +0530

    add inventory microservice

---
+++ inventryservice/src/main/java/com/example/inventoryservice/InventryserviceApplication.java
@@ -0,0 +1,13 @@
+package com.example.inventoryservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventryserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InventryserviceApplication.class, args);
+    }
+
+}
